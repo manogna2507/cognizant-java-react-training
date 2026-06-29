c0103f1723302ba13e9d704916bd2d93b74fc51a
manogna2507 <manogna.gampala2507@gmail.com>
2026-06-29 23:33:10 +0530

    Update SearchTest.java

--- exercise_2/SearchTest.java
+++ exercise_2/SearchTest.java
@@ -1,1 +1,25 @@
+public class SearchTest {
 
+    public static void main(String[] args) {
+
+        Product[] products = {
+                new Product(101, "Laptop", "Electronics"),
+                new Product(102, "Phone", "Electronics"),
+                new Product(103, "Shoes", "Fashion"),
+                new Product(104, "Watch", "Accessories"),
+                new Product(105, "Bag", "Fashion")
+        };
+
+        Product result1 = Search.linearSearch(products, 103);
+
+        if (result1 != null) {
+            System.out.println("Linear Search Found: " + result1.productName);
+        }
+
+        Product result2 = Search.binarySearch(products, 104);
+
+        if (result2 != null) {
+            System.out.println("Binary Search Found: " + result2.productName);
+        }
+    }
+}
