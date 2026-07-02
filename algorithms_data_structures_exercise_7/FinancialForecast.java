67507b2902c02a886f172cf00927e702f68bf712
manogna2507 <manogna.gampala2507@gmail.com>
2026-07-02 17:05:57 +0530

    Update FinancialForecast.java

--- algorithms_data_structures_exercise_7/FinancialForecast.java
+++ algorithms_data_structures_exercise_7/FinancialForecast.java
@@ -1,1 +1,12 @@
+public class FinancialForecast {
 
+    // Recursive method to calculate future value
+    public static double predictFutureValue(double currentValue, double growthRate, int years) {
+
+        if (years == 0) {
+            return currentValue;
+        }
+
+        return predictFutureValue(currentValue * (1 + growthRate), growthRate, years - 1);
+    }
+}
