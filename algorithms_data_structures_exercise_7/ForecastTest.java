d4699757709f423d702148595554d67c2d58deeb
manogna2507 <manogna.gampala2507@gmail.com>
2026-07-02 17:06:44 +0530

    Update ForecastTest.java

--- algorithms_data_structures_exercise_7/ForecastTest.java
+++ algorithms_data_structures_exercise_7/ForecastTest.java
@@ -1,1 +1,16 @@
+public class ForecastTest {
 
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10;   // 10%
+        int years = 5;
+
+        double futureValue = FinancialForecast.predictFutureValue(currentValue, growthRate, years);
+
+        System.out.printf("Current Value : %.2f%n", currentValue);
+        System.out.println("Growth Rate  : 10%");
+        System.out.println("Years        : " + years);
+        System.out.printf("Future Value : %.2f%n", futureValue);
+    }
+}
